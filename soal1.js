d23f03872fbd181e4339b38bb80349bd3c7c693b
RitaKomariah11 <76770662+RitaKomariah11@users.noreply.github.com>
2023-10-01 11:08:49 +0700

    Jawaban Tugas Pertemuan 3 Rita Komariah

---
+++ soal1.js
@@ -0,0 +1,19 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Masukkan bilangan genap: ", (input) => {
+  const x = parseFloat(input);
+
+  if (x < 0 || x % 2 !== 0) {
+    console.log("Tidak bisa input bilangan negatif atau bilangan ganjil");
+  } else {
+    const result = Math.sqrt(x);
+    console.log(`Akar pangkat 2 dari ${x} adalah: ${result}`);
+  }
+
+  rl.close();
+});
