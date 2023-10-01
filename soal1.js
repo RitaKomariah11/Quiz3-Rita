29b44804bed1f2e0d9e36944ef9abae78e735dc6
RitaKomariah11 <76770662+RitaKomariah11@users.noreply.github.com>
2023-10-01 11:19:14 +0700

    Add files via upload

--- soal1.js
+++ soal1.js
@@ -8,8 +8,10 @@ const rl = readline.createInterface({
 rl.question("Masukkan bilangan genap: ", (input) => {
   const x = parseFloat(input);
 
-  if (x < 0 || x % 2 !== 0) {
-    console.log("Tidak bisa input bilangan negatif atau bilangan ganjil");
+  if (x < 0) {
+    console.log("Tidak bisa input bilangan negatif");
+  } else if (x % 2 !== 0) {
+    console.log("Tidak bisa input bilangan ganjil");
   } else {
     const result = Math.sqrt(x);
     console.log(`Akar pangkat 2 dari ${x} adalah: ${result}`);
